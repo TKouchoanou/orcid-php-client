9c2b8c8f8237414e8d36c71f929674f4a98c711a
TKouchoanou <64207866+TKouchoanou@users.noreply.github.com>
2020-06-24 14:33:53 +0200

    Update OAwork.php

--- src/Work/OAwork.php
+++ src/Work/OAwork.php
@@ -6,12 +6,17 @@ namespace Orcid\Work;
 
  abstract  class OAwork
 {
-
      const ID_TYPE = 'idType';
      const ID_VALUE = 'idValue';
      const ID_URL = 'idUrl';
-     const CITATION_FORMAT=['formatted-unspecified', 'bibtex', 'ris', 'formatted-apa', 'formatted-harvard', 'formatted-ieee', 
-                            'formatted-mla', 'formatted-vancouver', 'formatted-chicago'];
+     const EXTERNAL_ID_RELATION_TYPE=['self', 'part-of'];
+     const CITATION_FORMATS=['formatted-unspecified', 'bibtex', 'ris', 'formatted-apa', 'formatted-harvard', 'formatted-ieee', 'formatted-mla', 'formatted-vancouver', 'formatted-chicago'];
+     const LANGAGE_CODES = ['en', 'ab', 'aa', 'af', 'ak', 'sq', 'am', 'ar', 'an', 'hy', 'as', 'av', 'ae', 'ay', 'az', 'bm', 'ba', 'eu', 'be', 'bn', 'bh', 'bi', 'bs', 'br', 'bg', 'my', 'ca', 'ch', 'ce', 'zh_CN', 'zh_TW', 'cu',
+         'cv', 'kw', 'co', 'cr', 'hr', 'cs', 'da', 'dv', 'nl', 'dz', 'en', 'eo', 'et', 'ee', 'fo', 'fj', 'fi', 'fr', 'fy', 'ff', 'gl', 'lg', 'ka', 'de', 'el', 'kl', 'gn', 'gu', 'ht', 'ha', 'iw', 'hz', 'hi', 'ho', 'hu', 'is',
+         'io', 'ig', 'in', 'ia', 'ie', 'iu', 'ik', 'ga', 'it', 'ja', 'jv', 'kn', 'kr', 'ks', 'kk', 'km', 'ki', 'rw', 'ky', 'kv', 'kg', 'ko', 'ku', 'kj', 'lo', 'la', 'lv', 'li', 'ln', 'lt', 'lu', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt'
+         , 'gv', 'mi', 'mr', 'mh', 'mo', 'mn', 'na', 'nv', 'ng', 'ne', 'nd', 'se', 'no', 'nb', 'nn', 'ny', 'oc', 'oj', 'or', 'om', 'os', 'pi', 'pa', 'fa', 'pl', 'pt', 'ps', 'qu', 'rm', 'ro', 'rn', 'ru', 'sm', 'sg', 'sa', 'sc', 'gd'
+         , 'sr', 'sn', 'ii', 'sd', 'si', 'sk', 'sl', 'so', 'nr', 'st', 'es', 'su', 'sw', 'ss', 'sv', 'tl', 'ty', 'tg', 'ta', 'tt', 'te', 'th', 'bo', 'ti', 'to', 'ts', 'tn', 'tr', 'tk', 'tw', 'ug', 'uk', 'ur', 'uz', 've', 'vi',
+         'vo', 'wa', 'cy', 'wo', 'xh', 'ji', 'yo', 'za', 'zu'];
 
      /**
       * @var string|int|
@@ -58,8 +63,12 @@ namespace Orcid\Work;
      }
 
      public function addExternalIdent($externalIdType,$externalIdValue,$externalIdUrl,$externalIdRelationship='self'){
+         if(!in_array(strtolower($externalIdRelationship),self::EXTERNAL_ID_RELATION_TYPE)){
+             throw new \Exception(" externalType : ".$externalIdType." , external value : ".$externalIdValue." , external relationship : ".$externalIdRelationship." . The External Ident type of relationship is not valid");
+         }
          $this->externals[]= new ExternalId($externalIdType,$externalIdValue,$externalIdUrl,$externalIdRelationship);
      }
+
      public function addNewExternalIdent(ExternalId $externalId){
          $this->externals[]= $externalId;
      }
@@ -69,7 +78,7 @@ namespace Orcid\Work;
       */
      public function setType($type)
      {
-       if(empty($type)||!is_string($type)){
+         if(empty($type)||!is_string($type)){
              throw new \Exception("The type of work must be string and not empity,you try to set the value which is not string or empity");
          }
          $this->type = $type;
@@ -85,10 +94,9 @@ namespace Orcid\Work;
       */
      public function setTitle($title, $translatedTitle='',$translatedTitleLanguageCode='')
      {
-        if(empty($title)||!is_string($title)){
-                throw new \Exception("The title of work must be string and not empity,you try to set the value which is not string or empity");
+         if(empty($title)||!is_string($title)){
+             throw new \Exception("The title of work must be string and not empity,you try to set the value which is not string or empity");
          }
-      
          $this->title = $title;
          $this->setTranslatedTitle($translatedTitle);
          $this->setTranslatedTitleLanguageCode($translatedTitleLanguageCode);
@@ -171,30 +179,30 @@ namespace Orcid\Work;
          $this->publicationDate = ['year'=>$year,'month'=>$month,'day'=>$day] ;
          return $this;
      }
-  
+
+
      /**
       * @return string
       */
-     public function getType(): string
+     public function getTitle()
      {
-         return $this->type;
+         return $this->title;
      }
 
      /**
       * @return string
       */
-     public function getTitle(): string
+     public function getType()
      {
-         return $this->title;
+         return $this->type;
      }
-  
-  
+
      /**
-     * @return array
-     */
-    public function getExternals(): array
-    {
-        return $this->externals;
-    }
+      * @return array
+      */
+     public function getExternals(): array
+     {
+         return $this->externals;
+     }
 
 }
